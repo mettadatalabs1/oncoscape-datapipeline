67184d15bdd1862db6f7b793bae3163ded32bab8
namelessnerd <karthik.gomadam@gmail.com>
2017-03-06 14:21:13 -0800

    first version of validation configurator

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+
+# just listing list of requires. will create a set up using these
+
+"""
+airflow>=1.7.1,
+numpy>=1.1,
+requests>=2.1,
+pymongo==3.4.0,
+pytest>=3.0,
+simplejson==3.10.0,
+tox==2.6
+"""
--- validators/validation_configurator.py
+++ validators/validation_configurator.py
@@ -1,13 +1,38 @@
-
+from yaml import load as load_yaml
 
 class ValidationConfigurator(object):
+    """
+    Given a file type, returns the validation function for the datatype and
+    for checking values, if present. Validations should be configured using the
+    validation_configs YAML file.
+    """
 
-    # configuration_dict (dict)
-    configuration_dict = {"validator":
-                            {"mut": lambda vco: vco.validate_type,
-                             "mut01": lambda vco: vco.validate_range_int,
-                             "cnv": lambda vco: vco.validate_,
+    # configs_dict (dict)
+    configs_dict = None
 
-                                       
-        
+    def __init__(self, file_datatype):
+        # a handler object for ValidationConfigurator.configs_dict
+        vc_configs_dict = ValidationConfigurator.configs_dict
+        if not configs_dict:
+            with open("validation_configs.yml","r") as config_file:
+                configs_dict = load_yaml(config_file)
+        try:
+            self.valid_datatype = vc_configs_dict[file_datatype][type]
+        except KeyError as e:
+            raise
+        try:
+            self.valid_values = vc_configs_dict[file_datatype][values]
+        except KeyError:
+            self.valid_values = None
 
+    def validate_datatype(self, value):
+        return type(value) is self.valid_datatype
+
+    def validate_value(self, value):
+        return value in self.valid_values
+
+    def validate(self, value):
+        valid_type = self.validate_datatype(value)
+        if self.valid_values:
+            valid_value = self.validate_value
+        return (valid_type, valid_value)
