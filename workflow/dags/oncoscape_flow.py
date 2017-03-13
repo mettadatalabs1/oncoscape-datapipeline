cbad0f0562cd40a5bf1020c51636504d1f0b2265
namelessnerd <karthik.gomadam@gmail.com>
2017-03-12 22:10:58 -0700

    changing directory name to avoid namespacing conflicts

--- workflow/dags/oncoscape_flow.py
+++ workflow/dags/oncoscape_flow.py
@@ -8,10 +8,10 @@ from datetime import datetime
 default_args = {
         "owner": "fred_hutch_sttr",
         "depends_on_past": False,
-        "start_date": datetime(2017, 3, 12),
+        "start_date": datetime(2017, 3, 1),
         "schedule_interval": "* * * * *",
 }
-"""        
+"""
 DAG for clinical and molecular data pipeline.
 Main tasks in this DAG are:
     1. Parse the job configuration json and return the job parameters
@@ -23,12 +23,12 @@ Main tasks in this DAG are:
     7. Log after each task and update the job status
 """
 # Declare the DAG artifacts
-#oncoscape_main_dag (airflow.DAG)   
+#oncoscape_main_dag (airflow.DAG)
 oncoscape_main_dag = DAG("OncoscapeMainDAG", default_args=default_args)
-#parse_job_config (airflow.operators.dummy_operator.DummyOperator)   
+#parse_job_config (airflow.operators.dummy_operator.DummyOperator)
 parse_job_config = DummyOperator(task_id="parse_job_config",
                                      dag=oncoscape_main_dag)
-#job_file_datatype_validate (airflow.operators.dummy_operator.DummyOperator)   
+#job_file_datatype_validate (airflow.operators.dummy_operator.DummyOperator)
 job_file_datatype_validate = PythonOperator(task_id="data_type_validator",
                             python_callable=None,
                             op_args=["dummy",],
@@ -36,4 +36,3 @@ job_file_datatype_validate = PythonOperator(task_id="data_type_validator",
 
 # create the DAG by setting the execution order for the operators
 parse_job_config.set_downstream(job_file_datatype_validate)
-
